66fcdbace3c231b9acd058cbb5339229697d4ed7
ArudoPE <aortizparodi@outlook.com>
2021-03-18 10:28:49 -0500

    Primeras clases

---
+++ functions.js
@@ -0,0 +1,9 @@
+//Declarate
+function myFunction() {
+    return 3;
+}
+//Expressive
+
+var myFunction = function(){
+    return a + b;
+}
---
+++ variables.js
@@ -0,0 +1,15 @@
+var name = "Oscar";
+
+//declarar
+var age;
+//inicializar
+age = 30;
+
+//Declarar e inicializar una varible
+var elements = ["Computer", "Cellphone"];
+
+var person = {
+    name: "Aldo",
+    age: 33
+};
+person;
